738d9516f14e3a3cdb392c3244a964db44327179
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-02-19 23:47:22 -0500

    update extract.py

--- data/extract.py
+++ data/extract.py
@@ -1,6 +1,7 @@
 # import necessary packages
 import pandas as pd
 import numpy as np
+from collections import Counter
 
 # read in data to pandas DataFrame
 path = 'data/oracle-cards-20230216100310.json'
@@ -17,8 +18,83 @@ keep_columns = ['name', 'mana_cost', 'cmc', 'type_line',
 # reassign DataFrame based on columns to keep
 df = df[keep_columns]
 
-# replace unescaped bullshit
+# replace unescaped characters
 df.replace(r"\n", " | ", inplace=True, regex=True)
+
+# dedupe based on card name and reset index
+df = df.drop_duplicates(subset = ['name']).reset_index(drop = True)
+
+# create mana_dict feature from mana cost
+df['mana_dict'] = df['mana_cost'].str.findall("\{([A-Z0-9/]+)\}").apply(lambda x: Counter(x) if isinstance(x, list) else Counter())
+
+# create mana_fields list
+mana_fields = list(df["mana_cost"].str.findall("\{([A-Z0-9/]+)\}").apply(lambda x: Counter(x) if isinstance(x, list) else Counter()).explode().unique())
+
+# define function to convert individual elements to int type
+def try_int(ele):
+    try:
+        return int(ele)
+    except:
+        return ele
+
+# sort mana_fields list and reconvert elements to strings
+mana_fields = sorted([try_int(i) for i in mana_fields], key = lambda x: (isinstance(x, str), x))
+mana_fields = [str(x) for x in mana_fields]
+rem_list = ['nan', 'Y', 'Z']
+mana_fields = [f for f in mana_fields if f not in rem_list]
+mana_fields.append('GENERIC')
+
+# create mana_df to generate mana features
+mana_df = pd.DataFrame(data = 0, columns = mana_fields, index = df.index)
+
+# loop over initial dataframe's mana_dict field and assign values from those dict values to mana_df
+for i in df.index:
+    if df.loc[i, 'mana_dict'] != np.nan:
+        j = list(df.loc[i, 'mana_dict'].items())
+        for n in j:
+            try:
+                mana_df.loc[i, 'GENERIC'] = int(n[0]) * n[1]
+            except:
+                mana_df.loc[i, n[0]] = n[1] 
+    else:
+        mana_df.loc[i, mana_fields] = 0
+
+# create second remove_list to remove integer mana costs
+rem_list_2 = ['0', '1', '2', '3', '4', '5', 
+              '6', '7', '8', '9', '10', '11', 
+              '12', '13', '14', '15', '16', 
+              '1000000', 
+              '2/B', '2/G', '2/R', '2/U', '2/W', 
+              'Y', 'Z']
+
+# create cleaned_mana_fields list for testing
+cleaned_mana_fields = [f for f in mana_fields if f not in rem_list_2]
+
+# drop irrelevant columns
+mana_df.drop(labels = rem_list_2, axis = 1, inplace = True)
+
+# merge initial dataframe with mana_df
+df = df.merge(mana_df, left_index = True, right_index = True)
+
+#####################################
+### BEGIN TESTING
+mismatched_cmc = []
+for i in df.index:
+    if df.loc[i, 'cmc'].astype('int') != int(sum(df.loc[i, cleaned_mana_fields]) - df.loc[i, 'X']):
+        mismatched_cmc.append({i: [df.loc[i, 'name'],
+                                    df.loc[i, 'cmc'].astype('int'), 
+                                    sum(df.loc[i, cleaned_mana_fields])]})
+
+print(len(mismatched_cmc))
+print(f"Mismatched CMC Ratio: {len(mismatched_cmc) / len(df) * 100}")
+
+for i in mismatched_cmc:
+    print(i)
+
+for i in df:
+    print(f"{i}: {df.loc[583, i]}")
+### END TESTING
+#####################################
 
 # write nodes
 df.to_csv("default_cards_nodes.csv", index=True, index_label="id")
