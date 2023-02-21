f45723b463a8299aa39fd4f3c74cf81eb5798bd8
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-02-21 13:16:40 -0500

    update extract.py

--- data/extract.py
+++ data/extract.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
+########## BEGIN DATA INPUT ##########
 # read in data to pandas DataFrame
 path = 'data/oracle-cards-20230216100310.json'
 df = pd.read_json(path)
@@ -24,6 +25,11 @@ df.replace(r"\n", " | ", inplace=True, regex=True)
 # dedupe based on card name and reset index
 df = df.drop_duplicates(subset = ['name']).reset_index(drop = True)
 
+########## END DATA INPUT ##########
+
+########## BEGIN FEATURE CREATION ##########
+
+### Feature 1: Mana Costs
 # create mana_dict feature from mana cost
 df['mana_dict'] = df['mana_cost'].str.findall("\{([A-Z0-9/]+)\}").apply(lambda x: Counter(x) if isinstance(x, list) else Counter())
 
@@ -53,31 +59,42 @@ for i in df.index:
         j = list(df.loc[i, 'mana_dict'].items())
         for n in j:
             try:
-                mana_df.loc[i, 'GENERIC'] = int(n[0]) * n[1]
+                mana_cost = int(n[0]) * n[1]
+                if df.loc[i, 'cmc'] != 0.0 | df.loc[i, 'cmc'] != np.nan:
+                    weighted_mana_cost = mana_cost / df.loc[i, 'cmc']
+                    mana_df.loc[i, 'GENERIC'] = weighted_mana_cost
+                else:
+                    mana_df.loc[i, 'GENERIC'] = 0.0
             except:
-                mana_df.loc[i, n[0]] = n[1] 
+                if df.loc[i, 'cmc'] != 0.0 & df.loc[i, 'cmc'] != np.nan:
+                    mana_cost = n[1]
+                    weighted_mana_cost = mana_cost / df.loc[i, 'cmc']
+                    mana_df.loc[i, n[0]] = weighted_mana_cost
+                else:
+                    mana_df.loc[i, n[0]] = 0.0
     else:
         mana_df.loc[i, mana_fields] = 0
 
-# create second remove_list to remove integer mana costs
-rem_list_2 = ['0', '1', '2', '3', '4', '5', 
-              '6', '7', '8', '9', '10', '11', 
-              '12', '13', '14', '15', '16', 
-              '1000000', 
-              '2/B', '2/G', '2/R', '2/U', '2/W', 
-              'Y', 'Z']
 
-# create cleaned_mana_fields list for testing
-cleaned_mana_fields = [f for f in mana_fields if f not in rem_list_2]
+# create second remove_list to remove integer mana costs
+rem_list_2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "1000000", "2/B", "2/G", "2/R", "2/U", "2/W"]
 
 # drop irrelevant columns
 mana_df.drop(labels = rem_list_2, axis = 1, inplace = True)
 
-# merge initial dataframe with mana_df
-df = df.merge(mana_df, left_index = True, right_index = True)
 
-#####################################
-### BEGIN TESTING
+
+
+for mdf in mana_df:
+    print(f"{df.loc[1, 'name']} {df.loc[1, 'cmc']} {mdf}: {mana_df.loc[1, mdf]}")
+
+
+
+
+
+########## BEGIN TESTING ##########
+# create cleaned_mana_fields list for testing
+cleaned_mana_fields = [f for f in mana_fields if f not in rem_list_2]
 mismatched_cmc = []
 for i in df.index:
     if df.loc[i, 'cmc'].astype('int') != int(sum(df.loc[i, cleaned_mana_fields]) - df.loc[i, 'X']):
@@ -93,9 +110,46 @@ for i in mismatched_cmc:
 
 for i in df:
     print(f"{i}: {df.loc[583, i]}")
-### END TESTING
-#####################################
+########## END TESTING ##########
 
+
+
+
+### Feature 2: Card Types
+type_list = list(df['type_line'].unique())
+perm_types = ['Land', 'Creature', 'Artifact', 'Enchantment', 'Planeswalker']
+non_perm_types = ['Instant', 'Sorcery']
+supertypes = ['Basic', 'Legendary', 'Snow', 'World']
+
+
+dual_card_types = []
+for t in type_list:
+    if "//" in t:
+        dual_card_types.append(t)
+    else:
+        continue
+
+subtypes = []
+for t in type_list:
+    if t not in dual_card_types:
+        card_string = t.split(sep = " — ")
+        subtypes.append(card_string[0])
+
+subtypes = list(np.unique(subtypes))
+subtypes
+
+########## END FEATURE CREATION ##########
+
+########## BEGIN JOINING FEATURES ##########
+### Join Features ###
+# merge initial dataframe with mana_df
+df = df.merge(mana_df, left_index = True, right_index = True)
+
+########## END JOINING FEATURES ##########
+
+
+
+########## BEGIN GRAPH CONVERSION ##########
 # write nodes
 df.to_csv("default_cards_nodes.csv", index=True, index_label="id")
 
@@ -124,7 +178,12 @@ def make_edges(dataframe):
 # make edges
 edges = make_edges(df) 
 
+########## END GRAPH CONVERSION ##########
+
+########## BEGIN DATA OUTPUT ##########
 # write out to .csv
 with open("default_cards_edges.csv", "w") as f:
     for e in edges:
         f.write(f"{','.join([str(n) for n in e])}""\n")
+
+########## END DATA OUTPUT ##########
